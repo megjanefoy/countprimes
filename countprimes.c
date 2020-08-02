2cbf4b5e5d74f4b048a5dc56df13811f299e30a5
Megan Jane Thompson <59883929+megjthom@users.noreply.github.com>
2020-08-02 16:25:01 -0500

    Update countprimes.c

--- countprimes.c
+++ countprimes.c
@@ -7,9 +7,6 @@
  * Semester: COP4338 - Spring 2021
  * Author Megan Jane Thompson
  *
- * I affirm that the revisions of this program is entirely
- * my own work and none of it is the work of any other person.
- *
  * This program creates n numbers of processes to find all
  * prime numbers 0 to 50000
  *
